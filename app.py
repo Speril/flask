ae77c08aeb1b5ca86035de5080bf4058ef11f284
Speril <star9moon@naver.com>
2021-06-08 23:16:08 +0900

    sign in 추가

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 from flask import Flask, render_template, request, url_for
+from werkzeug.utils import secure_filename
 app= Flask(__name__)
 
 @app.route('/')
 def index():
    return render_template("index.html")
-
-@app.route('/ct')
-def ct():
-   return render_template("ct.html")
 
 @app.route('/result')
 def result():
@@ -21,6 +18,22 @@ def map():
 def news():
    return render_template("news.html")
 
+@app.route('/ct')
+def ct():
+   return render_template("ct.html")
+
+@app.route('/signin')
+def signin():
+   return render_template("signin.html")
+
+#파일 업로드 처리 확인
+@app.route('/fileupload', methods = ['GET','POST'])
+def upload_file():
+       if request.method == 'POST':
+            f = request.files['file']
+            #저장 경로 + 파일명
+            f.save('./Uploader/'+ secure_filename(f.filename))
+            return 'file uploaded successfully!'
 
 if __name__ == "__main__":
    app.run(debug=True)
