efc02b7752fa587846813a4984d3260d5aab49c5
Su-eun157 <tndms1054@gmail.com>
2021-06-09 03:09:01 +0900

    model연동

--- app.py
+++ app.py
@@ -1,26 +1,55 @@
+from flask import Flask, request
+from flask.templating import render_template
+from werkzeug.utils import secure_filename
+import predict
+
+app = Flask(__name__)
+app.debug = True
+
 from flask import Flask, render_template, request, url_for
+from werkzeug.utils import secure_filename
 app= Flask(__name__)
 
 @app.route('/')
 def index():
-   return render_template("index.html")
+    return render_template("index.html")
+
 
 @app.route('/ct')
 def ct():
-   return render_template("ct.html")
+    return render_template("ct.html")
+
 
 @app.route('/result')
 def result():
-   return render_template("result.html")
+    return render_template("result.html", Result=result)
+
 
 @app.route('/map')
 def map():
-   return render_template("map.html")
+    return render_template("map.html")
+
 
 @app.route('/news')
 def news():
-   return render_template("news.html")
+    return render_template("news.html")
+
+
+@app.route('/signin')
+def signin():
+    return render_template("signin.html")
+
+
+# 파일 업로드 처리 확인
+@app.route('/fileupload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        image = request.files['image']
+        image.save('./Uploader/' + secure_filename(image.filename))
+        image_path = './test/' + str(image.filename)
+        transfer_img = predict.main(image_path)
+    return 'file uploaded successfully!'
 
 
 if __name__ == "__main__":
-   app.run(debug=True)
+    app.run(debug=True)
