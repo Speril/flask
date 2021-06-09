c31f971a2a10a373a8311430823395e47938e8d3
Su-eun157 <tndms1054@gmail.com>
2021-06-09 14:21:05 +0900

    commit pre

--- app.py
+++ app.py
@@ -2,13 +2,18 @@ from flask import Flask, request
 from flask.templating import render_template
 from werkzeug.utils import secure_filename
 import predict
+import os, re, glob
 
 app = Flask(__name__)
 app.debug = True
 
-from flask import Flask, render_template, request, url_for
-from werkzeug.utils import secure_filename
-app= Flask(__name__)
+
+def root_path():
+    '''root 경로 유지'''
+    real_path = os.path.dirname(os.path.realpath(__file__))
+    sub_path = "\\".join(real_path.split("\\")[:-1])
+    return os.chdir(sub_path)
+
 
 @app.route('/')
 def index():
@@ -20,9 +25,22 @@ def ct():
     return render_template("ct.html")
 
 
-@app.route('/result')
+@app.route('/result', methods=['GET', 'POST'])
 def result():
-    return render_template("result.html", Result=result)
+    image_dir = 'C:/Users/175767/PycharmProjects/flask/static/img/'
+    if request.method == 'POST':
+        root_path()
+
+        image = request.files['image']
+        image.save('./flask/static/img/' + str(image.filename))
+        image_path = (image_dir+str(image.filename))
+        image_type = str(image.filename).split('.')[1]
+        # img file
+        if image_type in ['jpg', 'JPG', 'jpeg', 'JPEG', 'png', 'PNG']:
+            print('Type is Image')
+            pre_result = predict.main(image_path)
+            # detected_img_path = '../static/img/' + str(detected_img.split('/')[-1])
+    return render_template("result.html", Result=pre_result, image_file=image_path)
 
 
 @app.route('/map')
@@ -41,15 +59,15 @@ def signin():
 
 
 # 파일 업로드 처리 확인
-@app.route('/fileupload', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        image = request.files['image']
-        image.save('./Uploader/' + secure_filename(image.filename))
-        image_path = './test/' + str(image.filename)
-        transfer_img = predict.main(image_path)
-    return 'file uploaded successfully!'
-
+# @app.route('/fileupload', methods=['GET', 'POST'])
+# def upload_file():
+#     if request.method == 'POST':
+#         image = request.files['image']
+#         image.save('./Uploader/' + secure_filename(image.filename))
+#         image_path = './test/' + str(image.filename)
+#         transfer_img = predict.main(image_path)
+#         transfer_img_path = str(transfer_img.split('/')[-1])
+#     return render_template("result.html", Result=result)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- predict.py
+++ predict.py
@@ -2,41 +2,34 @@ import os, re, glob
 import cv2
 import numpy as np
 from tensorflow.keras.models import load_model
-
-image_dir = '/test/'
-categories = ['COVID', 'Non-COVID']
-
-
-# def preprocess_image(image_path):
-# 	img = load_img(image_path, target_size=(img_height, img_width)) # (400, 381)
-# 	img = img_to_array(img) 			# (400, 381, 3)
-# 	img = np.expand_dims(img, axis=0) 	# (1, 400, 381, 3)
-# 	img = vgg19.preprocess_input(img)
-#
-# 	return img
-
+from tensorflow.keras.preprocessing.image import load_img, img_to_array, save_img
+from tensorflow.keras.applications import vgg19
 
 def Dataization(img_path):
     image_w = 112
     image_h = 112
-    img = cv2.imread(img_path)
+    ff = np.fromfile(img_path, np.uint8)
+    img = cv2.imdecode(ff, cv2.IMREAD_UNCHANGED)
+
     img = cv2.resize(img, None, fx=image_w / img.shape[1], fy=image_h / img.shape[0])
-    return img / 256
+    return (img/256)
 
 
-def main(image_path):
+def main(img_path):
+    image_dir = './static/img/test/'
+    categories = ['COVID', 'Non-COVID']
     src = []
     name = []
     test = []
 
-    for file in os.listdir(image_path):
+    for file in os.listdir(img_path):
         if file.find('.png') is not -1:
-            src.append(image_dir + file)
+            src.append(img_path + file)
             name.append(file)
-            test.append(Dataization(image_dir + file))
+            test.append(Dataization(img_path + file))
 
     test = np.array(test)
-    model = load_model('model/COVIDMD.h5')
+    model = load_model('C:/Users/175767/PycharmProjects/flask/model/COVIDMD.h5')
     predict = model.predict_classes(test)
 
     for i in range(len(test)):
